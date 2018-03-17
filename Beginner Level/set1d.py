26e81c22ff86d48e7fff654d0cb35facf5bdc263
ShathyaPeriaswamy <37336048+ShathyaPeriaswamy@users.noreply.github.com>
2018-03-17 14:51:00 +0530

    Create set1d.py

---
+++ Beginner Level/set1d.py
@@ -0,0 +1,6 @@
+inp=raw_input()
+my_inp=['a','e','i','o','u','q','w','r','t','y','u','i','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+if(inp in my_inp):
+	print "Alphabet"
+else:
+	print "No"
