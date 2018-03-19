55e3fddcc301be90d20f8d6cfce1a1e37ecd9437
ShathyaPeriaswamy <37336048+ShathyaPeriaswamy@users.noreply.github.com>
2018-03-19 19:35:22 +0530

    Update 12org.py

--- Beginner Level/set2/12org.py
+++ Beginner Level/set2/12org.py
@@ -1,12 +1,19 @@
 i=raw_input()
-a=int(i)
-z=a
-r=0
-while(a!=0):
-	b=a%10
-	r=(r*10)+b
-	a=a/10
-if(z==r):
-	print "yes"
+d=len(i)
+z=int(i)
+if i.isdigit():
+	a=int(i)
+	r=0
+	l=0
+	while(a!=0):
+		b=a%10
+		r=(r*0)+b**d
+		l+=r
+		a=a/10
+	if(z==l):
+		print "yes"
+	else:
+		print "no"
 else:
-	print "no"
+	print "invalid input"
+
