c5c01a13685a16754821fed2db764351706bb6bb
ShathyaPeriaswamy <37336048+ShathyaPeriaswamy@users.noreply.github.com>
2018-03-21 11:11:21 +0530

    Update 18.py

--- Beginner Level/set2/18.py
+++ Beginner Level/set2/18.py
@@ -1,16 +1,18 @@
 g,i=raw_input().split()
-d=len(i)
-z=int(i)
 if i.isdigit():
 	a=int(i)
-	r=0
-	l=0
-	for t in range(g,a+1):
-		while(a!=0):
-			b=t%10
+	y=int(g)
+	for x in range(y,a+1):
+		d=len(i)
+		r=0
+		l=0
+		e=x
+		while(e>0):
+			b=e%10
 			r=(r*0)+b**d	
 			l+=r
-			t=t/10
-		print l
+			e=e/10
+		if(x==l):
+			print l
 else:
 	print "invalid input"
