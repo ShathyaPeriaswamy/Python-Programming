a95bc0d4385e2e2ddee82c1f56f29ec6e924bfb9
ShathyaPeriaswamy <37336048+ShathyaPeriaswamy@users.noreply.github.com>
2018-04-10 18:56:44 +0530

    Update 24.py

--- Beginner Level/set3/24.py
+++ Beginner Level/set3/24.py
@@ -2,10 +2,9 @@ a=int(raw_input())
 b=raw_input().split()
 for i in range(a):
 	for j in range(a):
-		if(b[i]<b[j]):
+		if(int(b[i])<int(b[j])):
 			temp=b[i]
 			b[i]=b[j]
 			b[j]=temp
 for i in range(a):
 	print b[i],
-		
