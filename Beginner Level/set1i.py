eb519622cf7b93e10b68129f5c199589f65f61f6
ShathyaPeriaswamy <37336048+ShathyaPeriaswamy@users.noreply.github.com>
2018-04-13 18:19:23 +0530

    Create set1i.py

---
+++ Beginner Level/set1i.py
@@ -0,0 +1,18 @@
+a,b=raw_input().split()
+sum=0
+w=[]
+for i in range(0,int(a)):
+  r=raw_input()
+  print r
+  f=r
+  print f
+  g=f.replace(" ","")
+  print g
+  y=len(g)
+  print y
+  e=y
+if (e==a):
+  for j in range(int(b)):
+    t=r[j]
+    sum+=int(t)
+  print sum
