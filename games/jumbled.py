9223cf2ee658c0d639849f6346874a35763dcf92
ShathyaPeriaswamy <37336048+ShathyaPeriaswamy@users.noreply.github.com>
2018-04-27 22:54:28 +0530

    Create jumbled.py

---
+++ games/jumbled.py
@@ -0,0 +1,10 @@
+import random
+a=['dre','rwbon','kclab','uelb']
+r=random.choice(a)
+print r
+c=['red','brown','black','blue']
+b=raw_input()
+if(b in c):
+  print 'correct'
+else:
+  print 'wrong'
