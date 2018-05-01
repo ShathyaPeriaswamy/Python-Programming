71e796927465a0d294894cc0410e47b76e027654
ShathyaPeriaswamy <37336048+ShathyaPeriaswamy@users.noreply.github.com>
2018-05-01 15:28:57 +0530

    Update element k present_32.py

--- Player Level/set4/element k present_32.py
+++ Player Level/set4/element k present_32.py
@@ -1,8 +1,10 @@
 a,b=raw_input().split()
-for i in range(int(a)):
-  c=raw_input()
-  if b in c:
-    print 'Yes'
-  else:
-    print 'No'
-    
+if a.isdigit():
+  for i in range(int(a)):
+    c=raw_input()
+    if b in c:
+      print 'Yes'
+    else:
+      print 'No'
+else:
+  print 'invalid'
